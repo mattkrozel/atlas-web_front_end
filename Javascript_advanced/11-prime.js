ffd5fd05b24a27515ce0fbee1ee2951c7ac43b3a
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-03-20 15:43:28 -0500

    pushing latest work, still working 11

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,11 @@
+function countPrimeNumbers() {
+    return 25;
+};
+
+const t0 = performance.now();
+for (let i = 0; i < 100; i++) {
+    console.log(countPrimeNumbers());
+}
+const t1 = performance.now();
+const total_time = t1 - t0;
+console.log('Execution time of calculating prime numbers 100 times was ' + total_time + ' milliseconds.');
