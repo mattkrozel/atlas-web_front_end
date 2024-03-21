bb1c7a30c7a06a7243e43af51f579a806066cd9d
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-03-21 10:43:00 -0500

    completed task 13, binding

--- Javascript_advanced/13-bind_user.js
+++ Javascript_advanced/13-bind_user.js
@@ -1,17 +1,17 @@
 let user = {
-hobby: 'Calligraphy',
-favoriteSport: 'Hockey',
-astrologicalSign: 'Aries',
-firstName: 'Buillaume',
-lastName: 'Johns',
-location: 'Netherlands',
-occupation: 'Engineer'
+    hobby: 'Calligraphy',
+    favoriteSport: 'Hockey',
+    astrologicalSign: 'Aries',
+    firstName: 'Buillaume',
+    lastName: 'Johns',
+    location: 'Netherlands',
+    occupation: 'Engineer',
 };
 
 function logWelcomeUser(welcomeString) {
-    console.log('${welcomeString}, ${user.firstName}. Your occupation is: ${user.occupation}');    
-};
+    console.log(`${welcomeString}, ${user.firstName}. Your occupation is: ${user.occupation}`);    
+}
 
 let bindLogWelcomeUser = logWelcomeUser.bind(user);
-bindLogWelcomeUser('Welcome');
 bindLogWelcomeUser('Hello');
+
